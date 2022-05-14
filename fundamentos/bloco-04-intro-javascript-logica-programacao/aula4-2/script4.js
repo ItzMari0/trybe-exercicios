465f1bda8b5670fba13157c240a17dd97cd1df26
ItzMari0 <msajr@outlook.com>
2022-05-13 21:23:58 -0300

    Exercícios 1 a 5 resolvidos

---
+++ fundamentos/bloco-04-intro-javascript-logica-programacao/aula4-2/script4.js
@@ -0,0 +1,10 @@
+// Exercício pós aula 4
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; 
+    let soma = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9]; {
+    let mediaA = soma / 10;
+        console.log(mediaA);
+    if(mediaA > 20) 
+        console.log("Valor maior que 20");
+    else 
+        console.log("valor menor que 20");
+}
---
+++ fundamentos/bloco-04-intro-javascript-logica-programacao/aula4-2/script6.js
@@ -0,0 +1,11 @@
+// Exercício pós aula 6
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = 0;
+    for(let index = 0; index < numbers.length; index += 1) {
+    if(numbers[index] % 2 == 1) {
+        console.log("true");
+    }
+    else {
+        console.log("nenhum valor ímpar encontrado")
+    }    
+}
