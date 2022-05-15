dbb9ad65aa65d7ba40871aaf2e9d1f743ccd7645
ItzMari0 <msajr@outlook.com>
2022-05-15 20:33:31 -0300

    Exercícios da Aula 4-2 RESOLVIDOS!

--- fundamentos/bloco-04-intro-javascript-logica-programacao/aula4-2/script6.js
+++ fundamentos/bloco-04-intro-javascript-logica-programacao/aula4-2/script6.js
@@ -1,11 +1,9 @@
 // Exercício pós aula 6
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let impar = 0;
+let impar = [];
     for(let index = 0; index < numbers.length; index += 1) {
-    if(numbers[index] % 2 == 1) {
-        console.log("true");
+    if(numbers[index] % 2 != 0) {
+       impar.push(numbers[index]);
     }
-    else {
-        console.log("nenhum valor ímpar encontrado")
-    }    
 }
+    console.log(impar); 
