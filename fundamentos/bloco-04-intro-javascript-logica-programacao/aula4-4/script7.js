80f6ae7ddfdae977f848d773bbe6d40cfc5128b5
ItzMari0 <msajr@outlook.com>
2022-05-17 21:22:45 -0300

    Exercícios Parte I

---
+++ fundamentos/bloco-04-intro-javascript-logica-programacao/aula4-4/script7.js
@@ -0,0 +1,10 @@
+// Exercício pós aula 5
+let info = {
+    personagem: 'Margarida e Tio Patinhas',
+    origem: 'Pato Donald e Christmas on Bear Mountain, Dells Four Color Comic #178',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas',
+    recorrente: 'Ambos recorrentes',
+};
+    for(let index in info) {
+        console.log(info[index]);
+    }
---
+++ fundamentos/bloco-04-intro-javascript-logica-programacao/aula4-4/script9.js
@@ -0,0 +1,25 @@
+// Exercício pós aula 7
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+  
+  leitor.livrosFavoritos.push(
+  {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  }
+  );
+//   console.log(leitor.livrosFavoritos);
+  for(let index in leitor.livrosFavoritos){
+      console.log(leitor.livrosFavoritos[index]);
+  };
