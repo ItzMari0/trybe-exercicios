ee84e39e200ee345e4f9206e726c36d205219af1
ItzMari0 <msajr@outlook.com>
2022-06-28 20:07:02 -0300

    exercício 8 resolvido

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/aula8-5/script7.js
@@ -0,0 +1,5 @@
+const greet = (user, greeting = 'Hi') => `${greeting} ${user}`
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
