8367ad7cd79df7c249b275d854ddf9ec58c15edb
ItzMari0 <msajr@outlook.com>
2022-07-08 15:17:51 -0300

    resolvendo exercício 2

--- fundamentos/bloco-09-javascript-e-testes-assincronos/aula9-2/apiScript.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/aula9-2/apiScript.js
@@ -6,7 +6,7 @@ const fetchJoke = () => {
     headers: { 'Accept': 'application/json' }
   };
   
-  const result = fetch(API_URL, myObject)
+  fetch(API_URL, myObject)
   .then((response) => response.json())
   .then((data) => document.getElementById('jokeContainer').innerText = data.joke);
 };
